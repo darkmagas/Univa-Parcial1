f41f1bb28b4f5e19d8d55393fbc10b12a8ca408c
Hector Magaña <hector_magas@live.com>
2022-09-05 08:45:21 -0500

    First Class

---
+++ Assets/Scripts/Utils/Debugy.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugy : MonoBehaviour
+{
+    private void Awake()
+    {
+       Debug.Log("Awake"); 
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Debug.Log("Update");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("LateUpdate");
+    }
+}
