64f934e9a1a39182644481b58ac4e8c9b4c3078c
Hector Magaña <hector_magas@live.com>
2022-09-07 08:43:55 -0500

    SecondClass

---
+++ Assets/Scripts/Utils/SimpleMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 5, 
+            0, Input.GetAxis("Vertical") * Time.deltaTime * 5);
+    }
+}
