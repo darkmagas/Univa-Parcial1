edaca016b45532737beafda67c1db365b2b9b196
AranzaTorres <aranza.torresg23@gmail.com>
2022-09-08 08:59:59 -0500

    clase_02
    código para que el cubo se mueva

---
+++ Assets/Scripts/Utils/SampleMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //transform.position=new Vector3
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.W)) ;
+        //    }
+
+        //  Transform.position = new Vector3(Transform.position.x, y: transform.position.y+1*Time.deltaTime, z:0);
+
+
+        transform.Translate(x: Input.GetAxis("Horizontal") * 1 * Time.deltaTime,
+             y: Input.GetAxis("Vertical") * 1 * Time.deltaTime, z: 0);
+
+    
+    }
+}
