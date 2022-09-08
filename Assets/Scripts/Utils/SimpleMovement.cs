3424e25abddbf5abe456da459ebae1976a7609d7
Fernanda_Godoy <crank.fernanda@gmail.com>
2022-09-08 09:00:14 -0500

    PrimerClase

---
+++ Assets/Scripts/Utils/SimpleMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * 1 * Time.deltaTime, Input.GetAxis("Vertical") * 1 * Time.deltaTime, 0);
+
+
+
+        //if(Input.GetKey(KeyCode.W))
+        //{
+        //    transform.position = new Vector3(transform.position.x, y: transform.position.y + 1 * Time.deltaTime, 0);
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.position = new Vector3(x: transform.position.x + 1 * Time.deltaTime, transform.position.y, 0);
+        
+        //}
+    }
+}
