525744002810034088bef9f8828a30d397ec81c1
TonyBarajas <87187798+TonyBarajas@users.noreply.github.com>
2022-09-12 09:07:00 -0500

    week2_Class1

---
+++ Assets/Scripts/Gameplay/FollowPathMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPathMovement : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _wayPoints = new List<Transform>();
+    public string path = ""; /*esto ayuda a poder crear varios paths*/
+    public float stoppingDistance = 0.2f;
+    public float speed = 5f;
+
+    private int _currentWayPoint = 0;
+
+    //private int d; (solo yo tengo acceso) ameno que le pongas [SerializeField]
+    //public int a; (Todo el mundo tiene acceso)
+    //protected int b; (solo la descendencia tiene acceso)
+    //internal int c; (sabe alch)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var path = GameObject.Find(this.path); 
+        for (int i = 0; i < path.transform.childCount; i++)
+        {
+            _wayPoints.Add(path.transform.GetChild(i));
+        }
+
+        StartCoroutine(MoveToWayPoints());
+    }
+
+    //corrutina = algo que se actualiza constantemente pero lo puedo denteer
+
+    IEnumerator MoveToWayPoints()
+    {
+        var distance = Vector3.Distance(transform.position, _wayPoints[_currentWayPoint].position );
+        //calcular distancia entre a y b
+        while (Mathf.Abs(distance) > 0.2f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _wayPoints[_currentWayPoint].position,speed * Time.deltaTime); /*ordena que se desplce hacie el waypoint*/
+            distance = Vector3.Distance(transform.position, _wayPoints[_currentWayPoint].position); /*Se vuelve a insertar esta linea para poder hacer el loop de buscar el waypoint*/
+            yield return null; /*sirve para no trabar la compu*/
+        }
+        if (_currentWayPoint < _wayPoints.Count - 1)
+        {
+            _currentWayPoint++;
+            StartCoroutine(MoveToWayPoints());
+        }
+
+    }
+}
