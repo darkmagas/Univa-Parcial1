f0f06d6dfde999664ccbbabd7a339c4a2c007954
LiceMaDuck <47264390+LiceMaDuck@users.noreply.github.com>
2022-09-13 20:50:07 -0500

    Update

--- Assets/Scripts/FollowPathMovement.cs
+++ Assets/Scripts/FollowPathMovement.cs
@@ -5,60 +5,60 @@ using UnityEngine;
 public class FollowPathMovement : MonoBehaviour
 {
 
-    [SerializeField] private List<Tranform> _wayPoints = new List<Tranform>();
-    public string path = "",
+    [SerializeField] private List<Transform> _wayPoints = new List<Transform>();
+    public string path = "Path";
 
     public float stoppingDistance = 0.2f;
+
     public float speed = 5f;
 
-    //public int a ;
-
-    //protected int b;
-
     private int _currentWayPoint = 0;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        var path :GameObject = GameObject.Find(this.path)
+        var path = GameObject.Find(this.path);
 
-        for (int i = 0, i; i < path.tranform.childCount; i++)
+        for (int i = 0; i < path.transform.childCount; i++)
         {
-            _wayPoints.Add(item: path.tranform.GetChild(i));
+            _wayPoints.Add(item: path.transform.GetChild(i));
         }
-        StartCoroutine(routine: MovetoWayPoints());
+        StartCoroutine(MovetoWaypoint());
     }
 
-    IEnumerator MoveToWayPoints()
+     public IEnumerator MoveToWaypoint()
     {
-        var distance:float = Vector3.Distance(a: tranform.position, b: _wayPoints[_currentWayPoint].position);
+        var distance = Vector3.Distance(a: transform.position, b: _wayPoints[_currentWayPoint].position);
 
         while (Mathf.Abs(distance) > stoppingDistance)
         {
-            transform.position = Vector3.MoveTowards(current: tranform.position, target: _wayPoints[_currentWayPoint].position, maxDistanceDelta: speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, _wayPoints[_currentWayPoint].position, maxDistanceDelta: speed * Time.deltaTime);
 
-            distance: float = Vector3.Distance(a: tranform.position, b: _wayPoints[_currentWayPoint].position);
+            distance = Vector3.Distance(a: transform.position, b: _wayPoints[_currentWayPoint].position);
 
             //Le dice a la maquina que espere un Frame para volver a ejecutar
 
             yield return null;
         }
-        if (_currentWayPoint < _waypoints.Count - 1)
+        
+        
+        if (_currentWayPoint < _wayPoints.Count - 1)
         {
             _currentWayPoint++;
-            StartCoroutine(routine: MoveToWayPoints());
+            StartCoroutine(MoveToWaypoint());
 
         }
-        // Update is called once per frame
-
-
-
-    }
-    void Update()
-    {
         
+
+
+
     }
+    
+        
+ }
 
 
 
-}
+
