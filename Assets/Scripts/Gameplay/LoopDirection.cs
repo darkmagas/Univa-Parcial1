322377cba8af498b758816ee3f5e28ed8fc2cbf4
adyfdez <adyfdez123@outlook.com>
2022-09-19 08:44:39 -0500

    Clase 19 de Sep

---
+++ Assets/Scripts/Gameplay/LoopDirection.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopDirection : MonoBehaviour
+{
+    private Vector3 _lastPosition = Vector3.zero;
+    [SerializeField] private float _rotationSpeed = 150;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      _lastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = transform.position - _lastPosition;
+        var targetDirection = Vector3.RotateTowards(transform.forward, direction, 30, Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation,Quaternion.LookRotation(targetDirection), Time.deltaTime*_rotationSpeed);
+        _lastPosition = transform.position;
+    }
+}
