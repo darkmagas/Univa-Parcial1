3090b928e013ea508844ba6745e3a0fe2a9484e0
jorgecotero99 <107192302+jorgecotero99@users.noreply.github.com>
2022-09-19 08:45:57 -0500

    enemy
    trabajo enmigos

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]private int _health=100;
+    private int_currentHealth =100;
+    [SerializeField]´private UnityEvent<float>_
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
