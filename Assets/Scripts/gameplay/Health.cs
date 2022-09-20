c4de81ab6c2adaae9b53d02665135d205eae0759
DBLotus <108678061+DBLotus@users.noreply.github.com>
2022-09-20 08:53:24 -0500

    class 5

---
+++ Assets/Scripts/gameplay/Health.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int _health = 100;
+
+    private int _currentHealth = 100;
+    [SerializeField]private UnityEvent<float> _onHealthChanged = new ();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentHealth = _health;
+    }
+
+    public void RecieveDamage(int damage)
+    {
+        _currentHealth -= damage;
+        _onHealthChanged?.Invoke((float)_currentHealth / _health);
+    }
+
+}
