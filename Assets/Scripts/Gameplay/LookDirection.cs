f908cd81b515b71663c561ed722d09313e6ff707
LiceMaDuck <>
2022-09-20 07:19:23 -0500

    Avance

--- Assets/Scripts/Gameplay/LookDirection.cs
+++ Assets/Scripts/Gameplay/LookDirection.cs
@@ -8,18 +8,18 @@ public class LookDirection : MonoBehaviour
 
     void Start()
     {
-        //_lastPosition = transform.position;
+        _lastPosition = transform.position;
     }
 
 
     void Update()
     {
-        // var direction:Vector3 = transform.position - _lastPosition;
+         var direction:Vector3 = transform.position - _lastPosition;
 
-        // var targetDirection:Vector3 = Vector3.RotateTowards(current: transform.forward, target: direction, maxRadiantDelta: 30, maxMagnitudeDelta: Time.deltaTime);
+         var targetDirection:Vector3 = Vector3.RotateTowards(current: transform.forward, target: direction, maxRadiantDelta: 30, maxMagnitudeDelta: Time.deltaTime);
 
-        // transform.rotation = Quartenion.RotateTowards(from: transform.rotation, to: Quartenion.lookRotation(targetDirection), maxDegreesDelta: Time.deltaTime);
+         transform.rotation = Quartenion.RotateTowards(from: transform.rotation, to: Quartenion.lookRotation(targetDirection), maxDegreesDelta: Time.deltaTime);
 
-        //    _lastPosition = transform.position;
+            _lastPosition = transform.position;
     }
 }
