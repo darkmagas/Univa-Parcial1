f61dde336d033ff5cdbb24a667eea7e82ca360cd
Spoiledchip <0122110694@alumnos.univa.mx>
2022-09-20 08:24:09 -0500

    Clase dia 20 (sin codigo)

---
+++ Assets/Scripts/Gameplay/Health.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    [SerializeField]private int _healt = 100;
+    
+    
+    void Start()
+    {
+        
+    }
+
+   
+}
