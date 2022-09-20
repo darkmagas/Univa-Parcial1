23465d12c8689f45ba1c95e0a9056233aa160058
Ponycol <juegosdetony@hotmail.com>
2022-09-20 09:23:35 -0500

    Health

---
+++ Assets/Scripts/Gameplay/Health.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField] private int _health = 100;
+    private int _currentHealth = 100;
+    [SerializeField] private UnityEvent<float> _onHealthChanged = new();
+ 
+    private void Start()
+    {
+
+        _currentHealth = _health;
+
+
+    }
+
+    public void RecieveDamage (int damage)
+    {
+       // _currentHealth = _currentHealth - damage;
+        _currentHealth -= damage;
+        _onHealthChanged?.Invoke((float)_currentHealth / _health);
+
+
+
+    }
+
+
+
+}
+
+
