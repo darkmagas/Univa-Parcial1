8ad455386f0c390c67ea655f5d4ac8a363c66512
Ponycol <juegosdetony@hotmail.com>
2022-09-22 08:10:50 -0500

    Wave Configuration

---
+++ Assets/Scripts/Gameplay/Wave.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ [Serializable] public class Wave
+{
+
+    public int weakEnemy = 0;
+    public int mediumEnemy = 0;
+    public int strongEnemy = 0;
+    public int bossEnemy1 = 0;
+
+
+
+
+}
---
+++ Assets/Scripts/Gameplay/WaveConfiguration.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Ponycol/WaveConfig", fileName = "WaveConfiguration", order = 0)]
+
+public class WaveConfiguration : ScriptableObject
+
+{
+    public List<Wave> _waves;
+
+
+
+
+
+}
