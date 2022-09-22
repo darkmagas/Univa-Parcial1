94ff59aa1fade9a07ecbe246ab2ebf1cd7a78030
EmiliaACM <emiliaalejand18@gmail.com>
2022-09-22 08:07:10 -0500

    clase 5

---
+++ Assets/Scripts/Gameplay/WaveConfiguration.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Magas/WaveConfig" , fileName = "WaveConfiguration" ,order =0)]
+public class WaveConfiguration : ScriptableObject
+{
+    public List<Wave> _waves;
+}
