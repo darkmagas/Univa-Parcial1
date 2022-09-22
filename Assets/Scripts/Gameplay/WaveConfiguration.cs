bf07a6ba322d1c30227488cd6240cc56a9696cf8
Omar Ramirez <gamesplayer36040@gmail.com>
2022-09-22 08:07:47 -0500

    DatosWaves

---
+++ Assets/Scripts/Gameplay/WaveConfiguration.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Doragun/WaveConfig" , fileName ="WaveConfiguration" ,order =0)]
+
+public class WaveConfiguration : ScriptableObject
+{
+    public List<Wave> _waves;
+}
