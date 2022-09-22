faa69498750a292e79d98e8e3209dfc123daf9e8
JerryTaco <107119379+JerryTaco@users.noreply.github.com>
2022-09-22 08:07:03 -0500

    Clase05_
    Correciion barra de vida
    Game managment con time step
    Wave configuration

---
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField][Range(0,6)]private float timeStep = 1;
+
+    private void Start ()
+    {
+        Time.timeScale = timeStep;
+    }
+}
