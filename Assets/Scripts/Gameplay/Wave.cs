3d9f6a46f6083ae4419d058d90e7be8f3bfe157e
Alejandralco3D <73479725+Alejandralco3D@users.noreply.github.com>
2022-09-26 08:54:42 -0500

    Clase 26

---
+++ Assets/Scripts/Gameplay/Wave.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable] public class Wave
+{
+    public int weakEnemyCount;
+    public int midEnemyCount;
+    public int strongEnemyCount;
+}
