1181897caefa228b0e56f11a9499bf92e05afb78
spoiledchip <0122110694@alumnos.univa.mx>
2022-09-26 17:46:21 -0500

    Primera parte codigo

--- Assets/Scripts/Gameplay/FollowPathMovement.cs
+++ Assets/Scripts/Gameplay/FollowPathMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class FollowPathMovement : MonoBehaviour
 {
+
+    private List<Transform> _wayPoint = new List<Transform>();
+    private int _currentPoint = 0;
+    public float speed = 5f;
+    public float minDistance = 0.2f;
+    public string pathName = "Path";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        var wayPointParent = GameObject.Find("Path");
+        for (int i = 0; i > wayPointParent.transform.childCount; i++)
+        {
+            _wayPoint.Add(wayPointParent.transform.GetChild(i));
+        }
+
+        StartCoroutine(MoveToNextPoint());
     }
 
-    // Update is called once per frame
-    void Update()
+   private IEnumerator MoveToNextPoint()
     {
-        
+        var distance = Vector3.Distance(transform.position, _wayPoint[_currentPoint].position);
+        while (Mathf.Abs(distance) > minDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _wayPoint[_currentPoint].position, Time.deltaTime * speed);
+            distance = Vector3.Distance(transform.position, _wayPoint[_currentPoint].position);
+            yield return null;
+        }
+
+        if (_currentPoint < _wayPoint.Count - 1)
+        {
+            _currentPoint++;
+            StartCoroutine(MoveToNextPoint());
+        }
     }
+
 }
