ba4717cf2b69831dd08b5ef9e8e39d0f180e7a35
CH4SK <107192220+CH4SK@users.noreply.github.com>
2022-09-26 08:56:50 -0500

    26/09/22

---
+++ Assets/Scripts/Gameplay/EnemyManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private WaveConfiguration waveConfig;
+    private int _currentWave = 0;
+    [SerializeField] private string[] _pathNames;
+    [SerializeField] private Transform[] _spawnPoints;
+
+    private void Start()
+    {
+        _spawnPoints = new Transform[_pathNames.Length];
+        for (int i = 0; i < _pathNames.Length; i++)
+        {
+            var wayPointParent = GameObject.Find(_pathNames[i]);
+            var wayPoints = wayPointParent.GetComponentsInChildren<Transform>();
+            _spawnPoints[i] = wayPoints[0];
+        }
+
+        StartCoroutine(SpawnEnemies(_currentWave));
+    }
+
+    private IEnumerator SpawnEnemies(int waveID)
+    {
+        if (waveConfig._waves.Count <= waveID) yield break;
+        var wave = waveConfig._waves[waveID];
+
+        yield return StartCoroutine(SpawnEnemy(wave.weakEnemyCount, _weakEnemyPrefab));
+        yield return StartCoroutine(SpawnEnemy(wave.midEnemyCount, _midEnemyPrefab));
+         yield return StartCoroutine(SpawnEnemy(wave.strongEnemyCount, _strongEnemyPrefab));
+
+
+    }
+
+    private IEnumerator SpawnEnemy(int enemyCount, GameObject prefab)
+    {
+        for (int i = 0; i < enemyCount; i++)
+        {
+            var randomPathID = UnityEngine.Random.Range(0, _pathNames.Length);
+            Instantiate(prefab, _spawnPoints[randomPathID].position, Quaternion.identity);
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Gameplay/WaveConfiguration.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName =]
+
+public class WaveConfiguration : ScriptableObject
+{
+    public List<Wave> _waves = new ();
+}
