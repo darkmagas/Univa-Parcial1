d2987ed332360cf8c020ca34769c755f91a4cefc
adyfdez <adyfdez123@outlook.com>
2022-09-26 08:51:44 -0500

    26 de Sep

---
+++ Assets/Scripts/Gameplay/EnemyManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private WaveConfiguration waveConfig;
+    private int _currentWave = 0;
+    [SerializeField] private string[] _pathNames;
+    [SerializeField] private Transform[] _spawnPoints;
+
+    [SerializeField] private GameObject _weakEnemyPrefab;
+    [SerializeField] private GameObject _midEnemyPrefab;
+    [SerializeField] private GameObject _strongEnemyPrefab;
+    private void Start()
+    {
+        _spawnPoints = new Transform[_pathNames.Length];
+        for (int i = 0; i < _pathNames.Length; i++)
+        {
+            var wayPointParent = GameObject.Find(_pathNames[i]);
+            var wayPoints = wayPointParent.GetComponentsInChildren<Transform>();
+            _spawnPoints[i] = wayPoints[0];
+        }
+        StartCoroutine(SpawnEnemies(_currentWave));
+    }
+
+    private IEnumerator SpawnEnemies(int waveID)
+    {
+        if(waveConfig._waves.Count <= waveID) yield break;
+        var wave = waveConfig._waves[waveID];
+        yield return StartCoroutine(SpawnEnemy(wave.weakEnemyCount, _weakEnemyPrefab));
+        yield return StartCoroutine(SpawnEnemy(wave.midEnemyCount, _weakEnemyPrefab));
+        yield return StartCoroutine(SpawnEnemy(wave.strongEnemyCount, _weakEnemyPrefab));
+
+        yield return new WaitForSeconds(10f);
+        _currentWave++;
+        StartCoroutine(SpawnEnemies(_currentWave));
+    }
+
+ private IEnumerator SpawnEnemy(int EnemyCount, GameObject prefab)
+    {
+        for (int i = 0; i < EnemyCount; i++)
+        {
+            var randomPathID = UnityEngine.Random.Range(0, _pathNames.Length);
+            Instantiate(prefab, _spawnPoints[randomPathID].position, Quaternion.identity);
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+}
