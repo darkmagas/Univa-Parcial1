eb59735930eaa9b186610008eab33cb8eeec0549
NemesisD23 <dvelarderivera@aol.com>
2022-09-26 08:53:29 -0500

    addede scriprs

---
+++ Assets/Gameplay/WaveConfiguration.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WaveConfiguration", menuName = "DAN/WaveConfiguration", order = 0)]
+public class WaveConfiguration : ScriptableObject
+{
+
+    public List<Wave> _waves = new ();
+
+
+
+}
