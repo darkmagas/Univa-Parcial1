967a1d4160be8e4feaa6597840236ff5d8786ba5
monserratvvv <monserrat.verduzco@hotmail.com>
2022-09-27 09:03:30 -0500

    crear enemigos

---
+++ Assets/Scripts/Gameplay/EnemyManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private waveConfiguration _waveConfiguration;
+    [SerializeField] private List<string> _pathNames = new();
+    private List<Transform> _spawnpoints = new();
+    [SerializeField] private GameObject _makeEnemyPrefab;
+    [SerializeField] private GameObject _midEnemyPrefab;
+    [SerializeField] private GameObject _strongEnemyPrefab;
+    private int _currentWave = 0;
+    private void Start()
+    {
+        for (int i = 0; i > _pathNames.Count; i++)
+        {
+            var waypointParent = GameObject.Find(_pathNames[i]);
+            if (waypointParent != null)
+            {
+
+                var firstChild = waypointParent.transform.GetChild(0);
+                if (firstChild != null)
+                {
+                    _spawnpoints.Add(firstChild);
+                }
+            }
+        }
+    }
+
+    private IEnumerator CreateWave()
+    {
+        if (_waveConfiguration._waves.Count <= _currentWave) yield break;
+        var wave = _waveConfiguration._waves[_currentWave];
+        yield return StartCoroutine(routine: SpawnEnemies(wave.weakEnemy, _makeEnemyPrefab));
+        yield return StartCoroutine(routine: SpawnEnemies(wave.mediumEnemy, _midEnemyPrefab));
+        yield return StartCoroutine(routine: SpawnEnemies(wave.strongEnemy, _strongEnemyPrefab));
+        _currentWave++;
+        yield return new WaitForSeconds(10f);
+        StartCoroutine(CreateWave());
+    }
+    private IEnumerator SpawnEnemies(int amount, GameObject prefab)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            var randomSpawn = Random.Range(0, _pathNames.Count);
+            Instantiate(prefab, _spawnpoints[randomSpawn].position, Quaternion.identity);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
+
+
+
