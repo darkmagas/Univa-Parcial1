de805c186bf21029cab3aa07ea31928ee1380d4d
AnetteV19 <107191863+AnetteV19@users.noreply.github.com>
2022-09-26 19:06:19 -0500

    Correcciones

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]public class Wave
+[Serializable]
+public class Wave
 {
     public int weakEnemyCount;
     public int midEnemyCount;
--- Assets/Scripts/WaveConfiguration.cs
+++ Assets/Scripts/WaveConfiguration.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu (fileName = "WaveConfiguration", menuName = "Anette/WaveConfiguration", order = 0)]
+[CreateAssetMenu(fileName = "WaveConfiguration", menuName = "Anette/WaveConfiguration", order = 0)]
 
 public class WaveConfiguration : ScriptableObject
 {
-    public List<Wave> _waves = new();
+    public List<Wave> _waves = new ();
+
 }
