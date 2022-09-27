6664c6627241a42bf4390bb2df71fe0d16b4ab39
Valentina Gonzalez <daniela_vgo@hotmail.com>
2022-09-27 09:00:03 -0500

    clase_6

--- Assets/Scripts/Gameplay/FollowPathMovement.cs
+++ Assets/Scripts/Gameplay/FollowPathMovement.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class FollowPathMovement : MonoBehaviour
 {
+    private List<Transform> _wayPoints = new List<Transform> ();
+    private int _currentWayPoint = 0;
+    public float speed = 5f;
+    public float minDistance = 0.2f;
+    public string pathName = "Path";
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        var waypointParent = GameObject.Find("Path");
+        for (int i = 0; i < waypointParent.transform.childCount; i++)
+        {
+            _wayPoints.Add(waypointParent.transform.GetChild(i));
+        }
+
+        StartCoroutine(routine: MoveToNextWaypoint());
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator MoveToNextWaypoint()
+    
     {
-        
+
+        var distance = Vector3.Distance(transform.position, _wayPoints[_currentWayPoint].position);
+
+        while (Mathf.Abs(distance) > minDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _wayPoints[_currentWayPoint].position, Time.deltaTime * speed);
+            distance = Vector3.Distance(transform.position, _wayPoints[_currentWayPoint].position);
+            yield return null;
+
+        }
+
+        if (_currentWayPoint < _wayPoints.Count - 1)
+        {
+            _currentWayPoint++;
+            StartCoroutine(MoveToNextWaypoint());
+        }
     }
+
+   
+
 }
