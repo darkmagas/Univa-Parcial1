07ad6a4733a78550f8f7566c38579e80fbd497ab
AnetteV19 <107191863+AnetteV19@users.noreply.github.com>
2022-09-28 07:52:28 -0500

    Cambios

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,44 +1,45 @@
-[SerializeField] private GameObject _weakEnemyPrefab;
-[SerializeField] private GameObject _midEnemyPrefab;
-[SerializeField] private GameObject _strongEnemyPrefab;
-
-
-private void Start()
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyManager : MonoBehaviour
 {
-    _spawnPoints = new Transform[_pathNames.Length];
-    for (int i = 0; i < _pathNames.Length; i++)
+    [SerializeField] private WaveConfiguration waveConfig;
+    private int _currentWave = 0;
+    [SerializeField] private string[] _pathNames;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private GameObject _weakEnemyPrefab;
+    [SerializeField] private GameObject _midEnemyPrefab;
+    [SerializeField] private GameObject _strongEnemyPrefab;
+    private void Start()
     {
-        var wayPointParent = GameObject.Find(_pathNames[i]);
-        var wayPoints = wayPointParent.GetComponentsInChildren<Transform>();
-        _spawnPoints[i] = wayPoints[0];
+        _spawnPoints = new Transform[_pathNames.Length];
+        for (int i = 0; i < _pathNames.Length; i++)
+        {
+            var wayPointParent = GameObject.Find(_pathNames[i]);
+            var wayPoints = wayPointParent.GetComponentsInParent<Transform>();
+            _spawnPoints[i] = wayPoints[0];
+        }
+        StartCoroutine(SpawnEnemies(_currentWave));
     }
-
-    StartCoroutine(SpawnEnemies(_currentWave));
-
-}
-
-private IEnumerator SpawnEnemies(int waveID)
-{
-    if (waveConfig._waves.Count <= waveID) yield break;
-    var wave = waveConfig._waves[waveID];
-
-    yield return StartCoroutine(SpawnEnemy(wave.weakEnemyCount, _weakEnemyPrefab));
-    yield return StartCoroutine(SpawnEnemy(wave.midEnemyCount, _midEnemyPrefab));
-    yield return StartCoroutine(SpawnEnemy(wave.strongEnemyCount, _strongEnemyPrefab));
-
-    yield return new WaitForSeconds(10f);
-    _currentWave++;
-    StartCoroutine(SpawnEnemies(_currentWave));
-
-}
-private IEnumerator SpawnEnemy(int enemyCount, GameObject prefab)
-{
-    for (int i = 0; i < enemyCount; i++)
+    private IEnumerator SpawnEnemies(int waveID)
     {
-        var randomPathID = UnityEngine.Random.Range(0, _pathNames.Length);
-        Instantiate(original: prefab, _spawnPoints[randomPathID].position, Quaternion.identity);
-        yield return new WaitForSeconds(.5f);
+        if (waveConfig._waves.Count <= waveID) yield break;
+        var wave = waveConfig._waves[waveID];
+        yield return StartCoroutine(SpawnEnemy(wave.weakEnemyCount, _weakEnemyPrefab));
+        yield return StartCoroutine(SpawnEnemy(wave.midEnemyCount, _midEnemyPrefab));
+        yield return StartCoroutine(SpawnEnemy(wave.strongEnemyCount, _strongEnemyPrefab));
+        yield return new WaitForSeconds(10f);
+        _currentWave++;
+        StartCoroutine(SpawnEnemies(_currentWave));
     }
-}
+    private IEnumerator SpawnEnemy(int enemyCount, GameObject prefab)
+    {
+        for (int i = 0; i < enemyCount; i++)
+        {
+            var RandomPathID = UnityEngine.Random.Range(0, _pathNames.Length);
+            Instantiate(prefab, _spawnPoints[RandomPathID].position, Quaternion.identity);
+            yield return new WaitForSeconds(.5f);
+        }
+    }
 }
 
