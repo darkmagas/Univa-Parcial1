76d2a4c8fc2dc6922ade44468b48e2a54861c767
Valentina Gonzalez <daniela_vgo@hotmail.com>
2022-09-29 08:52:26 -0500

    clase_7

--- Assets/Scripts/Gameplay/EnemyManager.cs
+++ Assets/Scripts/Gameplay/EnemyManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using Magas.Utilities;
 
 public class EnemyManager : MonoBehaviour
 {
@@ -50,7 +51,8 @@ public class EnemyManager : MonoBehaviour
         {
 
             var randomSpawn = Random.Range(0, _pathNames.Count);
-            Instantiate(prefab, _spawnpoints [randomSpawn].position,Quaternion.identity);
+            //Instantiate(prefab, _spawnpoints [randomSpawn].position,Quaternion.identity);
+            EventDispatcher.Dispatch(signal: new SpawnObject(prefab,null,_spawnpoints[randomSpawn].position,Quaternion.identity, (gameObjectSpawn)=> { int rs = randomSpawn; string pathName = _pathNames[rs]; gameObjectSpawn.GetComponent<FollowPathMovement>().InitEnemy(pathName); }));
             yield return new WaitForSeconds(1);
 
         }
