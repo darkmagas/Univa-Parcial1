53eeecddc1defe8591f3e1cb59cef0249428cbd6
Ponycol <juegosdetony@hotmail.com>
2022-10-02 00:02:41 -0500

    Pruebas de TowerDefense 2D y placeholders

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
 
    [SerializeField] [Range (0,6)] private float timeStep = 1;
-
+   
 
     private void Update()
     {
@@ -16,8 +17,10 @@ public class GameManager : MonoBehaviour
 
 
 
+
     }
 
+    
 
 
 
