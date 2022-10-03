57972dd58b25829003cf559f77b8323686205076
adyfdez <adyfdez123@outlook.com>
2022-10-03 08:51:36 -0500

    Clase 3 Oct

---
+++ Assets/Scripts/Gameplay/OnEnemyDeath.cs
@@ -0,0 +1,12 @@
+using Magas.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnemyDeath : MonoBehaviour
+{
+  public void Die()
+    {
+        EventDispatcher.Dispatch(new DespawnObject(gameObject));
+    }
+}
