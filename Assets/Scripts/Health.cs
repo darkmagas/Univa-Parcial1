af242ce9e5a2bd08e05196d6a986b5dda3c2d6f5
161101 <andreabucioaceves@gmail.com>
2022-10-03 08:51:21 -0500

    Clase

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int _health = 100;
+    private int _currentHealth = 100;
+    [SerializeField] private UnityEvent<float> _onHealthChanged = new ();
+    [SerializeField] private UnityEvent _onDeath = new();
+    void OnEnable()
+    {
+        _currentHealth = _health;
+    }
+
+    public void ReceiveDamage(int damage)
+    {
+        _currentHealth =damage;
+        if(_currentHealth < 0)
+        {
+            _currentHealth = 0;
+        }
+        _onHealthChanged?.Invoke((float)_currentHealth / _health);
+        if(_currentHealth == 0)
+            _onDeath?.Invoke();
+    }
+}
