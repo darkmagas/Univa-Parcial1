1dc1626bbd247f6c05cb40e67776fc162b9b93a4
ArlethGM <107191632+ArlethGM@users.noreply.github.com>
2022-10-03 08:47:19 -0500

    7 Class

--- Assets/Scripts/Gameplay/Health.cs
+++ Assets/Scripts/Gameplay/Health.cs
@@ -8,19 +8,25 @@ public class Health : MonoBehaviour
     [SerializeField]private int _health = 100;
     private int _currentHealth = 100;
     [SerializeField]private UnityEvent<float> _onHealthChanged = new UnityEvent<float>();
+    [SerializeField]private UnityEvent _onDeath = new();
 
     // Start is called before the first frame update
-    void Start()
+    void onEnable()
     {
         _currentHealth = _health;
-
     }
 
-    
+
     public void ReceiveDamage (int damage)
     {
         _currentHealth -= damage;
+        if (_currentHealth < 0)
+        {
+            _currentHealth = 0;
+        }
         _onHealthChanged?.Invoke((float)_currentHealth / _health);
+        if (_currentHealth == 0)
+            _onDeath?.Invoke();
 
     }
 }
