307ed4dfacbfe339e8545c92c7e73a3816dcb089
Erickvda <erickvda01@gmail.com>
2022-10-04 08:37:56 -0500

    Gameover UI y restart

--- Assets/Scripts/Gamplay/GameManager.cs
+++ Assets/Scripts/Gamplay/GameManager.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField][Range(0,6)]private float timeStep = 1;
-
-    private void Update()
+    private static GameManager _instance;
+    public static GameManager Instance
     {
-        Time.timeScale = timeStep;
+        get
+        {
+            if (_instance == null)
+            {
+                var obj = FindObjectOfType<GameManager>();
+                if (obj != null)
+                {
+                    _instance = obj;
+                }
+                else
+                {
+                    GameObject newSingleton = new GameObject(name: "GameManager");
+                    _instance = newSingleton.AddComponent<GameManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+    [SerializeField][Range(0,6)]private float _gameSpeed = 1f;
+
+    public void ChangeSpeed(float speed)
+    {
+        _gameSpeed = speed;
+        Time.timeScale = _gameSpeed;
     }
 }
--- Assets/Scripts/Gamplay/Health.cs
+++ Assets/Scripts/Gamplay/Health.cs
@@ -8,9 +8,10 @@ public class Health : MonoBehaviour
     [SerializeField] private int _health = 100;
     private int _currentHealth = 100;
     [SerializeField] private UnityEvent<float> _onHealthChanged = new ();
+    [SerializeField] private UnityEvent _onDeath = new UnityEvent();
 
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
         _currentHealth = _health;
     }
@@ -18,7 +19,14 @@ public class Health : MonoBehaviour
     public void ReceiveDamage(int damage)
     {
        // _currentHealth = _currentHealth - damage;
+
         _currentHealth -= damage;
+        if(_currentHealth < 0)
+        {
+            _currentHealth = 0;
+        }
         _onHealthChanged?.Invoke((float)_currentHealth / _health);
+        if (_currentHealth == 0)
+            _onDeath?.Invoke();
     }
 }
