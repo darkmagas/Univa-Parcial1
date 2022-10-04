84937639efb8d495504a066c2767588c5ad2303b
Ponycol <juegosdetony@hotmail.com>
2022-10-04 08:52:10 -0500

    Life Bar y GameOver

---
+++ Assets/Scripts/Gameplay/GameOverUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private RectTransform _gameOverPanel;
+    public void OnGameOver()
+    {
+        _gameOverPanel.gameObject.SetActive(true);
+        GameManager.Instance.ChangeSpeed(0);
+    }
+
+    // Update is called once per frame
+    public void OnRestart()
+    {
+        GameManager.Instance.ChangeSpeed(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Scripts/Gameplay/Health.cs
+++ Assets/Scripts/Gameplay/Health.cs
@@ -9,8 +9,9 @@ public class Health : MonoBehaviour
     [SerializeField] private int _health = 100;
     private int _currentHealth = 100;
     [SerializeField] private UnityEvent<float> _onHealthChanged = new();
+    [SerializeField] private UnityEvent _onDeath = new();
  
-    private void Start()
+    void OnEnable()
     {
 
         _currentHealth = _health;
@@ -22,7 +23,15 @@ public class Health : MonoBehaviour
     {
        // _currentHealth = _currentHealth - damage;
         _currentHealth -= damage;
+
+        if (_currentHealth < 0)
+        {
+            _currentHealth = 0;
+        }
+
         _onHealthChanged?.Invoke((float)_currentHealth / _health);
+        if (_currentHealth == 0)
+            _onDeath?.Invoke();
 
 
 
