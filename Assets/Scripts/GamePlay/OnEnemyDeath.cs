f71c117d841551cf5774f2c2396715745d8490e6
xixi09 <ximesama03@gmail.com>
2022-10-04 08:53:52 -0500

    GameOver

---
+++ Assets/Scripts/GamePlay/OnEnemyDeath.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Magas.Utilities;
+
+public class OnEnemyDeath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Die()
+
+    {
+        EventDispatcher.Dispatch(new DespawnObject(gameObject));
+
+    }
+    
+}
