e6067a188154f6c9d9ed87c2cf711ab1ec4c0a17
Diana Rodriguez <itzel.rrgz@gmail.com>
2022-10-04 08:56:14 -0500

    GameOverUI

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -4,12 +4,38 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager _instance; //creamos variable global, pertenece a la clase no a ningun objeto
 
-    [SerializeField][Range(0,6)]private float timeStep = 1f; //para controlar la velocidad del juego y dar pausa.
+    public static GameManager Instance { //*
+        get
+    {
+            if (_instance == null)
+            {
+                var obj = FindObjectOfType<GameManager>();
+                if (obj != null)
+                {
+                    _instance = obj;                
+                }
+
+                else
+                {
+                    GameObject newSingleton = new GameObject("GameManager"); //singleton es basica y facil, no muy recomendable
+                    _instance = newSingleton.AddComponent<GameManager>();
+;                }
+            }
+            return _instance;
+    }
+    }
+    [SerializeField][Range(0,6)]private float _timeStep = 1f; //para controlar la velocidad del juego y dar pausa.
                                                              //serialized para modificar desde unity
                                                              //range para establecer un rango, igual se ve en unity
-    private void Update()
+                                                             // private void Update()
+                                                             //{ Time.timeScale = timeStep;} *esto lo cambiamos por el de abajo
+
+    public void timeStep(float speed)
     {
-        Time.timeScale = timeStep;
+        _timeStep = speed;
+        Time.timeScale = _timeStep;
     }
+    
 }
--- Assets/Scripts/Gameplay/Health.cs
+++ Assets/Scripts/Gameplay/Health.cs
@@ -8,9 +8,9 @@ public class Health : MonoBehaviour
     [SerializeField] private int _health = 100; //el serialize nos permite modificarlo desde unity.
     private int _currentHealth = 100;
     [SerializeField] private UnityEvent<float> _onHeatlthChanged = new(); //Lo que nos intereza es mandar el porcentaje de vida que va a tener nuestro enemigo. Para cuando la vida cambia
+    [SerializeField] private UnityEvent _onDeath = new(); //*evento de muerte para eñ game over.
 
-
-    void Start()
+    void Enable() //*cambiamos el start por el enable, se ejecuta cada que se prende el objeto.
     {
         _currentHealth = _health; //por si hay alguna modificacion en la salud se iguale. 
     }
@@ -18,6 +18,13 @@ public class Health : MonoBehaviour
     public void ReciveDamage(int damage)
     {
         _currentHealth -= damage; //es un chort cut de "_currentHealth - damage" y asi obtener el restante de vida.
+        if (_currentHealth < 0) // *si la vida lega a menos cero lo igualamos a 0 para que no pasen cosas raras.
+        {
+            _currentHealth = 0; 
+        }
         _onHeatlthChanged?.Invoke((float)_currentHealth / _currentHealth); // "?" es para ver si existe el pastel, de ser el caso se invoca el metodo, de lo contrario no se invoca. 
+
+        if (_currentHealth == 0)
+            _onDeath?.Invoke(); //cuado nada mas es 1 linea de codigo en el if se puede escribir sin corchetes
     }
 }
