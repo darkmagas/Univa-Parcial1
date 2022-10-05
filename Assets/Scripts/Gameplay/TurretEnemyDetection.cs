5c6a5d5d438e3172a8f84c3fb43f426ce2ba4880
Paw <kemonitodios@gmail.com>
2022-10-05 08:50:03 -0500

    clase 051022
    clase del cinco de octubre del 2022

---
+++ Assets/Scripts/Gameplay/TurretEnemyDetection.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretEnemyDetection : MonoBehaviour
+{
+    private GameObject _detectedEnemy = null;
+    [SerializeField] private Transform _turretPivot = null;
+    [SerializeField] private float _maxDistance = 1f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy")&& _detectedEnemy == null) 
+        {
+            _detectedEnemy = other.gameObject;
+        }
+
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //  if (other.CompareTag("Enemy")&& _detectedEnemy == other.gameObject) 
+    //    {
+    //        _detectedEnemy = null;
+    //    }
+    //}
+
+    private void Update()
+    {
+        if (_detectedEnemy != null)
+        {
+            var direction = _detectedEnemy.transform.position - _turretPivot.position - transform.parent.position;
+            var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+            _turretPivot.rotation = targetRotation;
+
+            var distance = Vector3.Distance(_detectedEnemy.transform.position, transform.parent.position);
+            if (Mathf.Abs(distance) > _maxDistance)
+            {
+                _detectedEnemy = null;
+            }
+        }
+    }
+}
