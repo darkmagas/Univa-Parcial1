f0d04c622143340af8c5801316be093aeb93cada
AnetteV19 <107191863+AnetteV19@users.noreply.github.com>
2022-10-05 08:50:26 -0500

    5 de Octubre

---
+++ Assets/Scripts/Gameplay/TurretEnemyDetection.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretEnemyDetection : MonoBehaviour
+{
+    private GameObject _detectedEnemy = null;
+    [SerializeField] private Transform _turretPivot = null;
+    [SerializeField] private float _maxDistance = 1f; 
+
+        private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy") && _detectedEnemy == null)
+        {
+            _detectedEnemy = other.gameObject; 
+        }
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
+    //    {
+    //        _detectedEnemy = null;
+    //   }
+    // }
+
+    private void Update()
+    {
+        if(_detectedEnemy != null)
+        {
+            var direction = _detectedEnemy.transform.position - _turretPivot.parent.position;
+            direction.y = 0; //con este comando las torretas nunca voltearan hacia arriba o  abajo
+            var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+            _turretPivot.rotation = targetRotation;
+
+            var distance = Vector3.Distance(_detectedEnemy.transform.position,
+                transform.parent.position);
+
+            if(Mathf.Abs(distance) > _maxDistance)
+            {
+                _detectedEnemy = null;
+            }
+        }
+    }
+}
