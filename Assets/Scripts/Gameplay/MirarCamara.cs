1e12c4de8c1160f0adab6382db303b32579ddd18
Diana Rodriguez <itzel.rrgz@gmail.com>
2022-10-05 04:34:00 -0500

    LookAt

---
+++ Assets/Scripts/Gameplay/MirarCamara.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirarCamara : MonoBehaviour
+{
+    public Transform ObjSeguir;
+    void Update()
+    {
+        //Transform.LookAt(ObjSeguir);
+    }
+}
