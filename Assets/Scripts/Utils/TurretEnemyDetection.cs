c914450da3dcdfb7a04bea783c35c9365a11a0ac
SergioGudiC <107191617+SergioGudiC@users.noreply.github.com>
2022-10-05 08:50:43 -0500

    Clase 05/10/22

---
+++ Assets/Scripts/Utils/TurretEnemyDetection.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretEnemyDetection : MonoBehaviour
+{
+    private GameObject _detectedEnemy = null;
+    [SerializeField] private Transform _turretPivot = null;
+    [SerializeField] private float _maxDistance = 1f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy") && _detectedEnemy == null)
+        {
+            _detectedEnemy = other.gameObject;
+        }
+    }
+   // private void OnTriggerExit(Collider other)
+    //{
+      //  if (other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
+       // {
+        //    _detectedEnemy = null;
+     //   }
+    //}
+    private void Update()
+    {
+        if (_detectedEnemy != null)
+        {
+            var direction = _detectedEnemy.transform.position - transform.parent.position;
+            direction.y = 0;
+            var targetRotation = Quaternion.LookRotation(forward:direction, upwards:Vector3.up);
+
+            var distance = Vector3.Distance(_detectedEnemy.transform.position, transform.parent.position);
+
+            if (Mathf.Abs(distance)> _maxDistance)
+            {
+                _detectedEnemy = null;
+            }
+        }
+    }
+
+
+
+}
