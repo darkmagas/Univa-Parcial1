c2fb92378bde048530e607b878de1fb5e00f80e0
Omar Ramirez <gamesplayer36040@gmail.com>
2022-10-06 08:45:58 -0500

    Torreta

---
+++ Assets/Scripts/Gameplay/TurretEnemyDetection.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretEnemyDetection : MonoBehaviour
+{
+    private GameObject _detectedEnemy = null;
+    [SerializeField] private Transform _turretPivot = null;
+
+    private void Update()
+    {
+        if (_detectedEnemy == null) return;
+
+        var direction = _detectedEnemy.transform.position - _turretPivot.position;
+        var targetRotation = Quaternion.LookRotation(forward: direction, upwards: Vector3.up);
+
+        _turretPivot.rotation = targetRotation;
+    }
+    private void OnTriggerEnter(Collider other)
+        
+    {
+     if(other.CompareTag("Enemy"))
+        {
+            _detectedEnemy = other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+      if(other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
+        {
+            _detectedEnemy = null;
+        }
+    }
+}
+
