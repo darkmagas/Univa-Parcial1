c2f7faba74c25dd20e2beca4ff139270912b609b
Valentina González <daniela_vgo@hotmail.com>
2022-10-06 11:02:10 -0500

    clase_9

---
+++ Assets/Scripts/Gameplay/TurretEnemyDetection.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretEnemyDetection : MonoBehaviour
+{
+    private GameObject _detectedEnemy = null;
+    [SerializeField] private Transform _turretPivot = null;
+
+    private void Update()
+    {
+        if (_detectedEnemy == null) return;
+
+        var direction = _detectedEnemy.transform.position - _turretPivot.position;
+        var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+        _turretPivot.rotation = targetRotation;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag ("Enemy"))
+        {
+            _detectedEnemy = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
+        {
+            _detectedEnemy = null;
+        }
+    }
+
+
+}
