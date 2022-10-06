5fa74d4c0319f6e70ae00ab4b89f39ccaa03ac0c
xixi03 <ximesama03@gmail.com>
2022-10-06 08:55:34 -0500

    Turrent

---
+++ Assets/Scripts/GamePlay/TurrenEnemyDetection.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurrenEnemyDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject _detectedEnemy = null;
+    [SerializeField] private Transform _turrentPivot = null;
+
+    private void Update()
+    {
+        if (_detectedEnemy == null) return;
+
+        var direction = _detectedEnemy.transform.position - _turrentPivot.position;
+        var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+        _turrentPivot.rotation = targetRotation;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+
+    {
+        if (other.CompareTag("Enemy")&& _detectedEnemy == null)
+
+        {
+            _detectedEnemy = other.gameObject;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
+
+        {
+            _detectedEnemy = null;
+        }
+
+    }
+}
