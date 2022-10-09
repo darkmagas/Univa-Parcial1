41ed50af7ec5ecdeb5805a286b227f4dad4bd733
AnetteV19 <anettegordoa1919@gmail.com>
2022-10-09 17:48:59 -0500

    Cubo

--- Assets/Scripts/Utils/SimpleMovement.cs
+++ Assets/Scripts/Utils/SimpleMovement.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class SimpleMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         transform.Translate(Input.GetAxis("Horizontal")*1*Time.deltaTime,
             Input.GetAxis("Vertical")*1*Time.deltaTime,0);
