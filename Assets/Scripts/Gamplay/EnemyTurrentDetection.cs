46eae73067180da1d6cb4a794c662eee3624a472
Erickvda <erickvda01@gmail.com>
2022-10-13 08:00:37 -0500

    Clase 13/10/2022

--- Assets/Scripts/Gamplay/EnemyTurrentDetection.cs
+++ Assets/Scripts/Gamplay/EnemyTurrentDetection.cs
@@ -7,27 +7,37 @@ public class EnemyTurrentDetection : MonoBehaviour
     // Start is called before the first frame update
     private GameObject _detectedEnemy = null;
     [SerializeField] private Transform _turrentPivot = null;
+    [SerializeField] private float _maxDistance = 5f;
 
     private void Update()
     {
         if (_detectedEnemy == null) return;
-        Vector3 direction = _detectedEnemy.transform.position - _turrentPivot.position;
+        var direction = _detectedEnemy.transform.position - _turrentPivot.parent.position;
         var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
         _turrentPivot.rotation = targetRotation;
+        var distance= Vector3.Distance(_detectedEnemy.transform.position, transform.parent.position);
+
+        if (Mathf.Abs(distance) > 10f)
+        {
+            _detectedEnemy = null;
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnterStay(Collider other)
     {
         if (other.CompareTag("Enemy") && _detectedEnemy == null)
         {
             _detectedEnemy = other.gameObject;
         }
     }
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
-        {
-            _detectedEnemy = null;
-        }
-    }
+
+
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if(other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
+    //    {
+    //        _detectedEnemy = null;
+    //    }
+    //}
 }
