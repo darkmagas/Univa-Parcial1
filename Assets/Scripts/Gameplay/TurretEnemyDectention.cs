77895933572d84e73b426c816e7d9de51c43eb2a
Enyaaaaaa <enya_ramirez@hotmail.com>
2022-10-13 08:04:59 -0500

    Cambio en Script Turrent

--- Assets/Scripts/Gameplay/TurretEnemyDectention.cs
+++ Assets/Scripts/Gameplay/TurretEnemyDectention.cs
@@ -6,18 +6,25 @@ public class TurretEnemyDectention : MonoBehaviour
 {
     private GameObject _detectedEnemy = null;
     [SerializeField] private Transform _turrentPivot = null;
+    [SerializeField] private float _maxDistance = 5f;
 
 
     private void Update()
     {
         if (_detectedEnemy == null) return;
 
-        var direction = _detectedEnemy.transform.position - _turrentPivot.position;
+        var direction = _detectedEnemy.transform.position - transform.parent.position;
         var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
         _turrentPivot.rotation = targetRotation;
+        var distance = Vector3.Distance(_detectedEnemy.transform.position, transform.parent.position);
+
+        if(Mathf.Abs(distance) > 10f)
+        {
+            _detectedEnemy = null;
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
 
     {
         if(other.CompareTag("Enemy") && _detectedEnemy == null) 
@@ -25,12 +32,12 @@ public class TurretEnemyDectention : MonoBehaviour
             _detectedEnemy = other.gameObject;
         }
     }
-    private void OnTriggerExit(Collider other)
+   //private void OnTriggerExit(Collider other)
 
-    {
-        if (other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
-        {
-            _detectedEnemy = null;
-        }
-    }
+   // {
+   //  if (other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
+   //   {
+   //        _detectedEnemy = null;
+   //    }
+   // }
 }
