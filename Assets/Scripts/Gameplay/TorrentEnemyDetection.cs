3230e03876284088b65f7aaa4c58a5e78b32c9e3
Diana Rodriguez <itzel.rrgz@gmail.com>
2022-10-13 08:05:40 -0500

    TorretaMiraDerecho

--- Assets/Scripts/Gameplay/TorrentEnemyDetection.cs
+++ Assets/Scripts/Gameplay/TorrentEnemyDetection.cs
@@ -6,14 +6,22 @@ public class TorrentEnemyDetection : MonoBehaviour
 {
     private GameObject _detectedEnemy = null;
     [SerializeField] private Transform _torretPivot = null;
+    [SerializeField] private float _maxDistance = 5f; //* para limitar el rango en el que la torreta detecta al enemigo, voltee a ver
 
     private void Update()
     {
         if (_detectedEnemy == null) return;
 
-       var direction = _detectedEnemy.transform.position - _torretPivot.position; //restamos posicion para saber donde se encuentra el enemigo
+       var direction = _detectedEnemy.transform.position - _torretPivot.parent.position; //restamos posicion para saber donde se encuentra el enemigo
+
         var targetRotation = Quaternion.LookRotation(direction, Vector3.up); //al detectarla que gire unicamente en el pivote de arriba.
         _torretPivot.rotation = targetRotation;
+        var distance = Vector3.Distance(_detectedEnemy.transform.position, transform.parent.position); //para que la torreta siga a los enemigos
+                                                                                                       //para ver mas que el primero.
+        if(Mathf.Abs(distance) > 10f) //independientemente del tamaño de la esfera 
+        {
+            _detectedEnemy = null;
+        }
     }
     private void OnTriggerEnter(Collider other)
         
@@ -24,13 +32,14 @@ public class TorrentEnemyDetection : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag("Enemy") && _detectedEnemy == other.gameObject) //para que si el objeto que se tiene asignado sale del rango le 
-                                                                            //deje de disparar.
-        {
-            _detectedEnemy = null;
-        }
-    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if(other.CompareTag("Enemy") && _detectedEnemy == other.gameObject) //para que si el objeto que se tiene asignado sale del rango le 
+    //                                                                        //deje de disparar.
+    //    {
+    //        _detectedEnemy = null;
+    //    }} * lo comentamos porque modificamos el codigo de arriba.
+    
 
 }
