341b6ecef8e3a4104bc0192260ae086d62b361e4
aranza23 <aranza.torresg23@gmail.com>
2022-10-13 08:15:47 -0500

    Conf_torreta

--- Assets/GamePlay/TurrentEnemyDetection.cs
+++ Assets/GamePlay/TurrentEnemyDetection.cs
@@ -6,18 +6,27 @@ public class TurrentEnemyDetection : MonoBehaviour
 {
     private GameObject _detectEdenemy = null;
     [SerializeField] private Transform _turretPivot = null;
+    [SerializeField] private float _maxDistance = 5f; 
 
     private void Update() // dirigir el pivote de la torreta
     {
         if (_detectEdenemy == null) return;
-        var direction = _detectEdenemy.transform.position - _turretPivot.position; // se guarda la posicion del pivote del enemigo
+        var direction = _detectEdenemy.transform.position - transform.parent.position; // se guarda la posicion del pivote del enemigo
+        direction.y = 0;
         var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
         _turretPivot.rotation = targetRotation;  //la torreta esta rotando
+
+        var distance = Vector3.Distance(_detectEdenemy.transform.position, transform.parent.position);
+
+        if (Mathf.Abs(distance) > _maxDistance)
+        {
+            _detectEdenemy = null;
+        }
     }
 
 
 
-    private void OnTriggerEnter(Collider other) //asignar valor y disparar
+    private void OnTriggerStay(Collider other) //asignar valor y disparar
     {
         if (other.CompareTag("Enemy") && _detectEdenemy ==null) //si mi detected enemy es igual a nulo, agregale un valor. Estamos haciend una comparacion de valores
         {
@@ -25,11 +34,12 @@ public class TurrentEnemyDetection : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit(Collider other) //quitar valor y dejar de disparar
-    {
-        if (other.CompareTag("Enemy") && _detectEdenemy == other.gameObject)  
-        {
-            _detectEdenemy = null;
-        }
-    }
+
+//    private void OnTriggerExit(Collider other) //quitar valor y dejar de disparar
+//    {
+//        if (other.CompareTag("Enemy") && _detectEdenemy == other.gameObject)  
+//        {
+//            _detectEdenemy = null;
+//        }
+//    }
 }
