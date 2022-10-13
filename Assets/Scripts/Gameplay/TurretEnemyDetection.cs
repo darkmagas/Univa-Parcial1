cb807274108a70794f9ac3bb91dba3b4e7177b23
Hector Magaña <hector_magas@live.com>
2022-10-13 08:21:28 -0500

    turrets

--- Assets/Scripts/Gameplay/TurretEnemyDetection.cs
+++ Assets/Scripts/Gameplay/TurretEnemyDetection.cs
@@ -7,40 +7,40 @@ public class TurretEnemyDetection : MonoBehaviour
 {
     private GameObject _detectedEnemy = null;
     [SerializeField] private Transform _turretPivot = null;
-    [SerializeField] private float _maxDistance = 1f;
+    [SerializeField] private float _maxDistance = 5f;
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
+    {
+        if(_detectedEnemy == null) return;
+
+        var direction = _detectedEnemy.transform.position - transform.parent.position;
+        
+        var targetRotation = Quaternion.LookRotation(direction,
+            Vector3.up);
+        _turretPivot.rotation = targetRotation;
+        var distance = Vector3.Distance(_detectedEnemy.transform.position, transform.parent.position);
+
+        if (Mathf.Abs(distance) > _maxDistance)
+        {
+            _detectedEnemy = null;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Enemy") && _detectedEnemy == null)
         {
             _detectedEnemy = other.gameObject;
         }
+        
     }
 
-     // private void OnTriggerExit(Collider other)
-     // {
-     //     if (other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
-     //     {
-     //         _detectedEnemy = null;
-     //     }
-     // }
-
-    private void Update()
-    {
-        if (_detectedEnemy != null)
-        {
-            var direction = _detectedEnemy.transform.position - transform.parent.position;
-           // direction.y = 0;
-            var targetRotation = Quaternion.LookRotation(direction, 
-                Vector3.up);
-            _turretPivot.rotation = targetRotation;
-            
-            var distance = Vector3.Distance(_detectedEnemy.transform.position, 
-                transform.parent.position);
-            if (Mathf.Abs(distance) > _maxDistance)
-            {
-                _detectedEnemy = null;
-            }
-        }
-    }
+    // private void OnTriggerExit(Collider other)
+    // {
+    //     if (other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
+    //     {
+    //         _detectedEnemy = null;
+    //     }
+    // }
 }
