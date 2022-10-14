6aa59da0a9e6261b23289a47fc3c15829623b339
Daniel <dvelarderivera@aol.com>
2022-10-14 12:25:43 -0500

    UI added

---
+++ Assets/Back.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Back : MonoBehaviour
+{
+    
+    public void EscenaExit()
+    {
+
+        SceneManager.LoadScene("UI_Proyect_NI");
+
+    }
+
+}
