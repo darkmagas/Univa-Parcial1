69d64606ce11eaa8027746c0745e61256d1a65b0
TonyBarajas <87187798+TonyBarajas@users.noreply.github.com>
2022-10-14 22:47:31 -0500

    Más cambios
    -se añadieron los sprites de Alexandra
    -animaciones de todos los sprites
    -3 enemigos, 3 torretas
    -UI completa
    -Background
    -Store
    -numero de enemigos
    -puntos
    -healthbars
    -imagenes comprimidas

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(Camera.main.transform.position, Vector3.up); 
+    }
+
+  
+}
