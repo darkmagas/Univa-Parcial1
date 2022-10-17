4d4dea8ec0a492e019a0636525a48bdee695d0b9
soulsmak <107191930+soulsmak@users.noreply.github.com>
2022-10-17 08:54:42 -0500

    17/Oct
    disparos de la torretita con Raycast

---
+++ Assets/Scripts/Gameplay/RaycastShooting.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShooting : MonoBehaviour
+{
+    [SerializeField] private Transform _cannon = null;
+    [SerializeField] private float _maxRange = 10f;
+    [SerializeField] private int _damage = 10;
+    [SerializeField] private AudioSource _audioSource=null;
+    [SerializeField] private GameObject _impactEffect = null;
+    private void FixedUpdate()
+    {
+        var ray = new Ray(_cannon.position, _cannon.forward);
+
+        if (Physics.Raycast(ray, out var hit, _maxRange))
+        {
+            if (hit.collider.CompareTag("Enemy"))
+            {
+
+               if(!_audioSource.isPlaying)
+                    _audioSource.Play();
+                hit.collider.GetComponent<Health>().RecieveDamage(_damage);
+            }
+        }
+       
+    }
+}
