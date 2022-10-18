2a7507d095602d1a3a3bd63b51e21a10697f54c8
Xixi09 <ximesama03@gmail.com>
2022-10-18 08:54:48 -0500

    RayCast

---
+++ Assets/Scripts/GamePlay/RayCastShooting.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastShooting : MonoBehaviour
+{
+    [SerializeField] private Transform _cannon = null;
+    [SerializeField] private float _maxRange = 10f;
+    [Header("Shooting Setting")]
+    [SerializeField] private int _damage = 10;
+    [SerializeField] private AudioSource _audioSource = null;
+    [SerializeField] private GameObject _impactEffect = null;
+    [SerializeField] private float _shootingCD = 0.5f;
+    private float _currentCD = 0f;
+
+
+    // Update is called once per frame
+    private void FixedUpdate()
+
+    {
+        if (_currentCD <= 0)
+        {
+
+
+            var ray = new Ray(_cannon.position, _cannon.forward);
+
+            // RaycastHit hit;
+            if (Physics.Raycast(ray, out var hit, _maxRange))
+            {
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    if (!_audioSource.isPlaying)
+                        _audioSource.Play();
+
+                    hit.collider.GetComponent<Health>().ReceiveDamage(_damage);
+
+                    _currentCD = _shootingCD;
+                }
+            }
+        }
+    }
+    private void Update()
+    {
+        if (_currentCD > 0)
+            _currentCD -= Time.deltaTime;
+    }
+
+
+
+}
+
