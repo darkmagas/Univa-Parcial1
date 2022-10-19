3bcc50a318ebe969ce85f4e4c7f31d2b5c45c18a
SrTamborez <emilianopollo666@gmail.com>
2022-10-19 08:49:00 -0500

    clase 19/10
    clase 19/10 MainMenu

--- Assets/Scripts/Gameplay/RaycastShooting.cs
+++ Assets/Scripts/Gameplay/RaycastShooting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Magas.Utilities;
 
 public class RaycastShooting : MonoBehaviour
 {
@@ -11,24 +12,39 @@ public class RaycastShooting : MonoBehaviour
     [SerializeField] private int _damage = 10;
     [SerializeField] private GameObject _impactEffect = null;
     [SerializeField] private AudioSource _audioSource = null;
-   
+    [SerializeField] private float _shootingCD = 1f;
+    private float _currentCD = 0;
+    
     private void FixedUpdate()
     {
-        var ray = new Ray(_cannon.position, _cannon.forward);
-        if (Physics.Raycast(ray, out var hit, _maxRange))
+        if (_currentCD <= 0)
         {
-            if (hit.collider.CompareTag("Enemy"))
+            var ray = new Ray(_cannon.position, _cannon.forward);
+            if (Physics.Raycast(ray, out var hit, _maxRange))
             {
-                if (!_audioSource.isPlaying) 
-                    _audioSource.Play();
-                Debug.Log(hit.collider.name);
-                hit.collider.GetComponent<Health>().ReceiveDamage(_damage);
-            }
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    if (!_audioSource.isPlaying)
+                        _audioSource.Play();
+                    EventDispatcher.Dispatch(new SpawnObject(_impactEffect, null, hit.point, Quaternion.identity, null));
+                    
+                    hit.collider.GetComponent<Health>().ReceiveDamage(_damage);
+                    _currentCD = _shootingCD;
+                }
 
+            }
         }
+       
       
     }
 
+    private void Update()
+    {
+        if (_currentCD > 0)
+        {
+            _currentCD -= Time.deltaTime;
+        }
+    }
 
 
 
