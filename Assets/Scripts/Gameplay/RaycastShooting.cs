dba47a6b1f11619c4d472ca4b2a48182efc78103
Paw <kemonitodios@gmail.com>
2022-10-19 08:48:58 -0500

    clase 191022

--- Assets/Scripts/Gameplay/RaycastShooting.cs
+++ Assets/Scripts/Gameplay/RaycastShooting.cs
@@ -10,17 +10,37 @@ public class RaycastShooting : MonoBehaviour
     [SerializeField] private int _damage = 10;
     [SerializeField] private AudioSource _audioSource = null;
     [SerializeField] private GameObject _impactEffect = null;
+    [SerializeField] private float _shootingCD = 1f;
+    private float _currentCD = 0;
 
     private void FixedUpdate()
     {
+        if(_currentCD <= 0)
+        { 
         var ray = new Ray(_cannon.position, _cannon.forward);
-        if (Physics.Raycast(ray, out var hit, _maxRange))
-        {
-            if (hit.collider.CompareTag("Enemy"))
+            if (Physics.Raycast(ray, out var hit, _maxRange))
             {
-                if (!_audioSource.isPlaying)
-                    _audioSource.Play();
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    //if (!_audioSource.isPlaying)
+                        _audioSource.Play();
+
+                    EventDispatcher.Dispatch(new SpawnObject(_impactEffect, null, hit.point, Quaternion.identity, null));
+                    hit.collider.GetComponent<Health>().ReceiveDamage(_damage);
+                    _currentCD = _shootingCD;
+                }
             }
         }
     }
+
+    private void Update()
+    {
+    if (_currentCD > 0)
+        {
+            _currentCD -= Time.deltaTime;
+        }
+    }
+
 }
+
+
