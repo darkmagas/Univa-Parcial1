587749dfda9fc0ab29012589423445431d33f476
CH4SK <107192220+CH4SK@users.noreply.github.com>
2022-10-19 08:48:49 -0500

    19/10/22

--- Assets/Scripts/Gameplay/MainMenu.cs
+++ Assets/Scripts/Gameplay/MainMenu.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
