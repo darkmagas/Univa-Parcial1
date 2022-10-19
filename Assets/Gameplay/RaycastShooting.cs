511268579301ef291babfc3b8b83f6a7c256d57a
NemesisD23 <108678106+NemesisD23@users.noreply.github.com>
2022-10-19 08:50:37 -0500

    MainMenu
    19/10/22

--- Assets/Gameplay/RaycastShooting.cs
+++ Assets/Gameplay/RaycastShooting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Magas.Utilities;
 
 public class RaycastShooting : MonoBehaviour
 {
@@ -10,27 +11,49 @@ public class RaycastShooting : MonoBehaviour
     [Header("Sooting Setings")]
     [SerializeField] private int _damage = 10;
     [SerializeField] private AudioSource _audioSource = null;
-    [SerializeField] private GameObject _impactEffect = null;  
+    [SerializeField] private GameObject _impactEffect = null;
+    [SerializeField] private float _shootingCD = 1f;
+    private float _currentCD = 1f;
 
 
     private void FixedUpdate()
     {
-        Ray ray = new Ray(_cannon.position, _cannon.forward);
-
-        if (Physics.Raycast(ray, out var hit, _maxRange))
+        if(_currentCD <= 0)
         {
+            var ray = new Ray(_cannon.position, _cannon.forward);
 
-            if (hit.collider.CompareTag("Enemy"))
+            if (Physics.Raycast(ray, out var hit, _maxRange))
             {
 
-                if (!_audioSource.isPlaying)
-                    _audioSource.Play();
-                hit.collider.GetComponent<Health>().ReciveDamage(_damage);
+                if (hit.collider.CompareTag("Enemy"))
+                {
+
+                   // if (!_audioSource.isPlaying)
+                        _audioSource.Play();
+
+                    EventDispatcher.Dispatch(new SpawnObject(_impactEffect, null, hit.point, Quaternion.identity, null));
+
+                    hit.collider.GetComponent<Health>().ReciveDamage(_damage);
+                    _currentCD = _shootingCD;
+
+                }
 
             }
-
         }
         
+        
+    }
+
+    private void Update()
+    {
+        
+        if (_currentCD > 0)
+        {
+
+            _currentCD -= Time.deltaTime;
+
+        }
+
     }
 
 }
