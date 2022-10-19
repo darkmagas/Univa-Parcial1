ab0b86085fd911eb40ec6bd31b0b5dc73586e5f3
CH4SK <107192220+CH4SK@users.noreply.github.com>
2022-10-19 08:47:58 -0500

    19/10/22

--- Assets/Scripts/Gameplay/RaycastShooting.cs
+++ Assets/Scripts/Gameplay/RaycastShooting.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Magas.Utilities;
 
 public class RaycastShooting : MonoBehaviour
 {
@@ -11,21 +12,38 @@ public class RaycastShooting : MonoBehaviour
     [SerializeField] private int _damage = 10;
     [SerializeField] private AudioSource _audioSource = null;
     [SerializeField] private GameObject _impactEffect = null;
- /// <summary>
- ///   P== ------------- E 
- /// </summary>
+    [SerializeField] private float _shootingCD = 1f;
+    private float _currentCD = 0;
+    /// <summary>
+    ///   P== ------------- E 
+    /// </summary>
     private void FixedUpdate()
     {
-        var ray = new Ray(_cannon.position, _cannon.forward);
-        
-        if (Physics.Raycast(ray, out var hit, _maxRange))
+        if (_currentCD <= 0)
         {
-            if (hit.collider.CompareTag("Enemy"))
+            var ray = new Ray(_cannon.position, _cannon.forward);
+        
+            if (Physics.Raycast(ray, out var hit, _maxRange))
             {
-                if(!_audioSource.isPlaying)
-                    _audioSource.Play();
-                hit.collider.GetComponent<Health>().ReceiveDamage(_damage);
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    if(!_audioSource.isPlaying)
+                        _audioSource.Play();
+                    EventDispatcher.Dispatch(new SpawnObject(_impactEffect, null, hit.point, Quaternion.identity, null));
+
+                    hit.collider.GetComponent<Health>().ReceiveDamage(_damage);
+                    _currentCD = _shootingCD;
+                }
             }
+        }
+        
+    }
+
+    private void Update()
+    {
+        if(_currentCD > 0)
+        {
+            _currentCD -= Time.deltaTime;
         }
     }
 }
