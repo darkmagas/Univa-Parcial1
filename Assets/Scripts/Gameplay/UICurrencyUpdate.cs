1d065ae1282cc86c18bab7c3acc18502b0e37c30
SrTamborez <emilianopollo666@gmail.com>
2022-10-24 08:47:55 -0500

    clase 24/10
    clase del 24/10
    aplicamos torretas que disparan y cuestan para poderlas poner en escena

---
+++ Assets/Scripts/Gameplay/UICurrencyUpdate.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UICurrencyUpdate : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private string _currencyName = "Gil";
+
+    public void UpdateCurrency(int currency)
+    {
+        _text.SetText($"{currency} {_currencyName}");
+    }
+
+
+}
