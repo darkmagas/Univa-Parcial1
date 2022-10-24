48b9e6d8f0fd1b1b66261649d5118583b638168e
16112001 <andreabucioaceves@gmail.com>
2022-10-24 08:50:57 -0500

    Clase_Placement

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -25,12 +25,23 @@ public class GameManager : MonoBehaviour
         } 
     }
     [SerializeField] [Range(0,6)] private float _gameSpeed = 1f;
-    
+
+    private LevelCurrencyManager _levelCurrencyManager;
 
     // Update is called once per frame
     public void ChangeSpeed(float speed)
     {
         _gameSpeed = speed;
         Time.timeScale = _gameSpeed;
+    }
+
+    public void AddLevelCurrencyManager(LevelCurrencyManager levelCurrencyManager)
+    {
+        _levelCurrencyManager = levelCurrencyManager;
+    }
+
+    public bool TrySpendCurrency(int amount)
+    {
+        return _levelCurrencyManager.TrySpendCurrency(amount);
     }
 }
