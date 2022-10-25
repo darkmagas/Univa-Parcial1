5f29005335a07391aff01456e982f5ada011a6cd
monserratvvv <monserrat.verduzco@hotmail.com>
2022-10-25 07:45:32 -0500

    Cubos

--- Assets/Scripts/Gameplay/RaycastShooting.cs
+++ Assets/Scripts/Gameplay/RaycastShooting.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class RaycastShooting : MonoBehaviour
 {
     [SerializeField] private Transform _cannon = null;
-    [SerializeField] private float  _maxRange = 10f;
- 
+    [SerializeField] private float _maxRange = 10f;
+
     [Header("ShootingSettings")]
     [SerializeField] private int _damage = 10;
     [SerializeField] private AudioSource _audioSource = null;
@@ -15,18 +15,19 @@ public class RaycastShooting : MonoBehaviour
     private float _currentCD = 0f;
     private void FixedUpdate()
     {
-        if (_currentCD <=0)
-    {
-        var ray = new Ray(_cannon.position, _cannon.forward);
-        if (Physics.Raycast(ray, out var hit, _maxRange))
+        if (_currentCD <= 0)
         {
-            if (hit.collider.CompareTag("Enemy"))
+            var ray = new Ray(_cannon.position, _cannon.forward);
+            if (Physics.Raycast(ray, out var hit, _maxRange))
             {
-                if (!_audioSource.isPlaying)
-                    _audioSource.Play();
-                hit.collider.GetComponent<Health>().ReceiveDamage(_damage);
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    if (!_audioSource.isPlaying)
+                        _audioSource.Play();
+                    hit.collider.GetComponent<Health>().ReceiveDamage(_damage);
                 }
-        }
+            }
 
+        }
     }
 }
