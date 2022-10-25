07e6699107281f2da4991cc87d929b6415d2978d
spoiledchip <0122110694@alumnos.univa.mx>
2022-10-25 09:05:43 -0500

    Torreta cuesta dinero y se hace click para colocar
    25 de Octubre 2022

---
+++ Assets/Scripts/Gameplay/RayCastShooting.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Magas.Utilities;
+
+public class RayCastShooting : MonoBehaviour
+{
+    [SerializeField] private Transform _cannon = null;
+    [SerializeField] private float _maxRange = 10f;
+    [Header("Shooting Settings")]
+    [SerializeField] private int _damage = 10;
+    [SerializeField] private AudioSource _audioSource = null;
+    [SerializeField] private GameObject _impactEffect = null;
+    [SerializeField] private float _shootingCD = 0.5f;
+    private float _currentCD = 0.5f;
+    private void FixedUpdate()
+    {
+        if(_currentCD <= 0)
+        {
+            var ray = new Ray(_cannon.position, _cannon.forward);
+            if (Physics.Raycast(ray, out var hit, _maxRange))
+            {
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    _audioSource.Play();
+                    EventDispatcher.Dispatch(new SpawnObject(_impactEffect, null, hit.point, Quaternion.identity, null));
+                    hit.collider.GetComponent<Health>().ReceiveDamage(_damage);
+                    _currentCD = _shootingCD;
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (_currentCD > 0)
+            _currentCD -= Time.deltaTime;
+    }
+}
