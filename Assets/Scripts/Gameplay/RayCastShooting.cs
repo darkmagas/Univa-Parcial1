8fcc2e769d455edff48b2876510c556a2a0eb383
Omar Ramirez <gamesplayer36040@gmail.com>
2022-10-25 09:03:29 -0500

    Curency
    JU

--- Assets/Scripts/Gameplay/RayCastShooting.cs
+++ Assets/Scripts/Gameplay/RayCastShooting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Magas.Utilities;
 
 public class RayCastShooting : MonoBehaviour
 {
@@ -22,20 +23,23 @@ public class RayCastShooting : MonoBehaviour
         var ray = new Ray(origin: _cannon.position, direction: _cannon.forward);
         if (Physics.Raycast(ray, out var hit, _maxRange))
         {
-            if(hit.collider.CompareTag("Enemy"))
+            if (hit.collider.CompareTag("Enemy"))
             {
                 if (!_audioSource.isPlaying)
                     _audioSource.Play();
+
+                EventDispatcher.Dispatch(new SpawnObject(_impactEffect, null, hit.point, Quaternion.identity, null));
                 hit.collider.GetComponent<Health>().ReceiveDamage(_damage);
 
                 _currentCD = _shootingCD;
             }
         }
     }
-}
 
-private void Update()
-{
-    if (_currentCD > 0)
-        _currentCD -= Time.deltaTime;
+
+    private void Update()
+    {
+        if (_currentCD > 0)
+            _currentCD -= Time.deltaTime;
+    }
 }
--- Assets/Scripts/Layer.cs
+++ Assets/Scripts/Layer.cs
@@ -32,6 +32,7 @@ namespace Univa
         public const int TouchGround = 7;
         public const int Enemy = 8;
         public const int DepthVisual = 9;
+        public const int Placement = 10;
         /// <summary>
         /// Use this type in place of layer or layer mask values in code / scripts.
         /// </summary>
@@ -53,6 +54,7 @@ namespace Univa
             public const int TouchGround = 128;
             public const int Enemy = 256;
             public const int DepthVisual = 512;
+            public const int Placement = 1024;
         }
     }
 }
