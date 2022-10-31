85fda47633fe80f69d879abeee0791ff9013fc46
16112001 <andreabucioaceves@gmail.com>
2022-10-31 08:53:18 -0600

    Clase_adelanto

---
+++ Assets/Cats_and_Rabbits/Scripts/TurretManager.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretManager : MonoBehaviour
+{
+    
+    [Serializable]public struct TurretConfig
+    {
+        public GameObject turret;
+        public int cost;
+    }
+
+    [SerializeField] private List<TurretConfig> _turretConfigs = new();
+
+    public (GameObject turret, int cost)GetTurretConfig(int index)
+    {
+        return (_turretConfigs[index].turret, _turretConfigs[index].cost);
+    }
+}
