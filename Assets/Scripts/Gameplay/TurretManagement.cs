e98d1caa9d1bb433d1c765e9c0049aee6632d536
SrTamborez <emilianopollo666@gmail.com>
2022-10-31 08:30:22 -0600

    clase 31/10
    clase 31/10

---
+++ Assets/Scripts/Gameplay/TurretManagement.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TurretManagement : MonoBehaviour
+{
+    [Serializable]public struct TurretConfig
+    {
+        public GameObject turret;
+        public int cost;
+    }
+
+    [SerializeField] private List<TurretConfig> _turretConfigs = new List<TurretConfig>();
+    
+    public  (GameObject turret, int cost) GetTurretConfig(int index)
+    {
+        return (_turretConfigs[index].turret, _turretConfigs[index].cost);
+    }
+}
--- Assets/Scripts/Gameplay/TurretPlacement.cs
+++ Assets/Scripts/Gameplay/TurretPlacement.cs
@@ -6,24 +6,37 @@ using Magas.Utilities;
 public class TurretPlacement : MonoBehaviour
 {
 
-    [SerializeField] private GameObject turretPrefab;
+    private GameObject _turretPrefab;
+    private int _cost = 0;
+
+    public void OnTurretChange((GameObject prefab, int cost)turret)
+    {
+        _cost = turret.cost;
+        _turretPrefab = turret.prefab;
+    }
 
     private void Update()
     {
+        if (_turretPrefab == null) return;
         if (Input.GetMouseButtonDown(0))
         {
-            if (GameManager.Instance.TrySpendCurrency(10))
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, Mathf.Infinity, LayerMask.GetMask("Placement")))
             {
-                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out var hit, Mathf.Infinity, LayerMask.GetMask("Placement")))
+                var hitTransform = hit.collider.transform;
+                if (!hitTransform.GetComponent<TurretSlot>().IsOccupied)
                 {
+                    if (GameManager.Instance.TrySpendCurrency(10))
+                    {
 
-                    var hitTransform = hit.collider.transform;
-                    EventDispatcher.Dispatch(new SpawnObject(turretPrefab, null, hitTransform.position, Quaternion.identity, null));
-                    var positionVector = new Vector3(hitTransform.position.x, 0, hitTransform.position.z);
+                        var positionVector = new Vector3(hitTransform.position.x, 0, hitTransform.position.z);
+                        EventDispatcher.Dispatch(new SpawnObject(_turretPrefab, null, positionVector, Quaternion.identity, null));                       
+                        hitTransform.GetComponent<TurretSlot>().SetStatus(true);
 
+                    }
                 }
             }
+
         }
     }
 
