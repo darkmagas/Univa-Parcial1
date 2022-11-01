c82288105445bd4784cf20b2921185fa36a64491
NickMeat <Nickname2088@gmail.com>
2022-11-01 08:02:49 -0600

    b

--- Assets/Scripts/Utils/EventDispatcher.cs
+++ Assets/Scripts/Utils/EventDispatcher.cs
@@ -1,20 +1,25 @@
-﻿using System;
+﻿+ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Magas.Utilities {
-    public static class EventDispatcher {
-        public static T GetOrAddComponent<T>(this GameObject model) where T: Component {
-           return model.GetComponent<T>() ?? model.AddComponent<T> () as T;
+namespace Magas.Utilities
+{
+    public static class EventDispatcher
+    {
+        public static T GetOrAddComponent<T>(this GameObject model) where T : Component
+        {
+            return model.GetComponent<T>() ?? model.AddComponent<T>() as T;
         }
-        
-        public static bool IsEqualVector(this Vector3 t, Vector3 e) {
+
+        public static bool IsEqualVector(this Vector3 t, Vector3 e)
+        {
             return Math.Abs(t.x - e.x) < 0.1 && Math.Abs(t.y - e.y) < 0.1 && Math.Abs(t.z - e.z) < 0.1;
         }
-        
+
         private static readonly Dictionary<Type, SignalDelegate> Events = new Dictionary<Type, SignalDelegate>();
 
-        public static void Subscribe<T>(SignalDelegate callback) where T : ISignal {
+        public static void Subscribe<T>(SignalDelegate callback) where T : ISignal
+        {
             var type = typeof(T);
             if (!Events.ContainsKey(type))
                 Events.Add(type, null);
@@ -22,12 +27,14 @@ namespace Magas.Utilities {
             Events[type] += callback;
         }
 
-        public static void Unsubscribe<T>(SignalDelegate callback) where T : ISignal {
+        public static void Unsubscribe<T>(SignalDelegate callback) where T : ISignal
+        {
             var type = typeof(T);
             if (Events.ContainsKey(type)) Events[type] -= callback;
         }
 
-        public static void Dispatch<T>(T signal) where T : ISignal {
+        public static void Dispatch<T>(T signal) where T : ISignal
+        {
             var type = typeof(T);
             if (!Events.ContainsKey(type)) return;
 
