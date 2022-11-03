c96fc139b6d4b96f7f3eeaa7f011e192698bbf58
NickMeat <113994938+NickMeat@users.noreply.github.com>
2022-11-03 07:50:54 -0600

    d

--- Assets/Scripts/Layer.cs
+++ Assets/Scripts/Layer.cs
@@ -59,20 +59,5 @@ namespace Univa
     }
 }
 
-Scripts / Menu / MainMenu.cs
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour
-{
-    public void LoadScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-    }
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-}
+
 
