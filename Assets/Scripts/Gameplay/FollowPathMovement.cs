47bd0c33c46a408b6d3f9a61a6f3334e112022a0
Infinity6868 <Infinity6868@users.noreply.github.com>
2022-11-07 08:40:11 -0600

    CLASE 11 / 07 / 22

--- Assets/Scripts/Gameplay/FollowPathMovement.cs
+++ Assets/Scripts/Gameplay/FollowPathMovement.cs
@@ -10,13 +10,21 @@ public class FollowPathMovement : MonoBehaviour
     public float speed = 5f;
     public float minDistance = 0.2f;
     private Vector3 _originalPosition;
+    private bool _isDeath = false;
 
     private void OnEnable()
     {
+        _isDeath = false;
         _originalPosition = transform.position;
         _wayPoints.Clear();
         _currentWayPoint = 0;
         GameManager.Instance.AddEnemy(1);
+    }
+
+    public void OnDeath()
+    {
+        _isDeath = true; 
+        StopCoroutine(MoveToNextWaypoint());
     }
 
     private void OnDisable()
@@ -40,7 +48,7 @@ public class FollowPathMovement : MonoBehaviour
     {
         var distance = Vector3.Distance(transform.position,
             _wayPoints[_currentWayPoint].position);
-        while (Mathf.Abs(distance) > minDistance)
+        while (Mathf.Abs(distance) > minDistance && _isDeath)
         {
             transform.position = Vector3.MoveTowards(transform.position,
                 _wayPoints[_currentWayPoint].position, Time.deltaTime * speed);
@@ -49,7 +57,7 @@ public class FollowPathMovement : MonoBehaviour
             yield return null;
         }
 
-        if (_currentWayPoint < _wayPoints.Count - 1)
+        if (_currentWayPoint < _wayPoints.Count - 1 && !_isDeath)
         {
             _currentWayPoint++;
             StartCoroutine(MoveToNextWaypoint());
