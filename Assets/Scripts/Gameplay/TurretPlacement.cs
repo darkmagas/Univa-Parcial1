e256c5edace5c47dde1fceb7890fa79c7bd5408d
Andrea Bucio Aceves <andreabucioaceves@gmail.com>
2022-11-06 22:30:56 -0600

    Arreglos

--- Assets/Scripts/Gameplay/TurretPlacement.cs
+++ Assets/Scripts/Gameplay/TurretPlacement.cs
@@ -5,21 +5,32 @@ using Magas.Utilities;
 
 public class TurretPlacement : MonoBehaviour
 {
-    [SerializeField] private GameObject turretPrefab;
+    [SerializeField] private GameObject _turretPrefab;
+    private int _cost = 0;
+    public void OnTurretChange((GameObject prefab, int cost) turret)
+    {
+        _cost = turret.cost;
+        _turretPrefab = turret.prefab;
+    }
     void Update()
     {
+        if (_turretPrefab == null) return;
+
         if (Input.GetMouseButtonDown(0))
         {
-            if (GameManager.Instance.TrySpendCurrency(10))
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, Mathf.Infinity, LayerMask.GetMask("Placement")))
             {
-                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if(Physics.Raycast(ray, out var hit, Mathf.Infinity, LayerMask.GetMask("Placement")))
+                var hitTransform = hit.collider.transform;
+                if (!hitTransform.GetComponent<TurretSlot>().IsOccupied)
                 {
-                    var hitTransform = hit.collider.transform;
-                    var positionVectpr = new Vector3(hitTransform.position.x, 0, hitTransform.position.z);
-                    EventDispatcher.Dispatch(new SpawnObject(turretPrefab, null, hitTransform.position, Quaternion.identity, null));
+                    if (GameManager.Instance.TrySpendCurrency(_cost))
+                    {
+                        var positionVector = new Vector3(hitTransform.position.x, 0, hitTransform.position.z);
+                        EventDispatcher.Dispatch(new SpawnObject(_turretPrefab, null, hitTransform.position, Quaternion.identity, null));
+                        hitTransform.GetComponent<TurretSlot>().SetStatus(true);
+                    }
                 }
-            
             }
         }
     }
