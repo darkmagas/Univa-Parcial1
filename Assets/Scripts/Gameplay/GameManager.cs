0b4bae3ef5c3b77d1f3c4fc9c0c0855b5d563478
blackcookiee <crank.fernanda@gmail.com>
2022-11-09 21:59:13 -0600

    trabajoCasa

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Rendered Background;
     private static GameManager _instance;
     public static GameManager Instance
     {
