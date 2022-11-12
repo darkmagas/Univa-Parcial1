40cb293fab4d9d6414b76436910353743e6203c5
Edgar Manzano <mauricioem2000@hotmail.com>
2022-11-12 13:07:20 -0600

    Main Menu

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
