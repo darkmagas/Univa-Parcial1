7402be019704be722d76308f273e2480aff21fe8
Alejandralco3D <73479725+Alejandralco3D@users.noreply.github.com>
2022-11-11 23:52:19 -0600

    Codigo turret
    correcion del codigo de la torreta

--- Assets/Scripts/Gameplay/TurretEnemyDetection.cs
+++ Assets/Scripts/Gameplay/TurretEnemyDetection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,40 +7,32 @@ public class TurretEnemyDetection : MonoBehaviour
 {
     private GameObject _detectedEnemy = null;
     [SerializeField] private Transform _turretPivot = null;
-    [SerializeField] private float _maxDistance = 1f; 
-
-        private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Enemy") && _detectedEnemy == null)
-        {
-            _detectedEnemy = other.gameObject; 
-        }
-    }
-
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.CompareTag("Enemy") && _detectedEnemy == other.gameObject)
-    //    {
-    //        _detectedEnemy = null;
-    //   }
-    // }
+    [SerializeField] private float _maxDistance = 5f;
 
     private void Update()
     {
-        if(_detectedEnemy != null)
+        if (_detectedEnemy == null) return;
+
+        var direction = _detectedEnemy.transform.position - transform.parent.position;
+
+        var targetRotation = Quaternion.LookRotation(direction,
+            Vector3.up);
+        _turretPivot.rotation = targetRotation;
+        var distance = Vector3.Distance(_detectedEnemy.transform.position, transform.parent.position);
+
+        if (Mathf.Abs(distance) > _maxDistance)
         {
-            var direction = _detectedEnemy.transform.position - _turretPivot.parent.position;
-            direction.y = 0; //con este comando las torretas nunca voltearan hacia arriba o  abajo
-            var targetRotation = Quaternion.LookRotation(direction, Vector3.up);
-            _turretPivot.rotation = targetRotation;
-
-            var distance = Vector3.Distance(_detectedEnemy.transform.position,
-                transform.parent.position);
-
-            if(Mathf.Abs(distance) > _maxDistance)
-            {
-                _detectedEnemy = null;
-            }
+            _detectedEnemy = null;
         }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Enemy") && _detectedEnemy == null)
+        {
+            _detectedEnemy = other.gameObject;
+        }
+
     }
 }
