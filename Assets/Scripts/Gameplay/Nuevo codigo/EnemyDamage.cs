d57c2fe66edc22a70565240638ea3126df2cc990
Alejandralco3D <73479725+Alejandralco3D@users.noreply.github.com>
2022-11-11 21:13:13 -0600

    Codigo nuevo para dano

---
+++ Assets/Scripts/Gameplay/Nuevo codigo/EnemyDamage.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D colision)
+    {
+        if(colision.tag == "Player")
+        {
+            PlayerLife.life -= DoDamage;
+        }
+    }
+}
