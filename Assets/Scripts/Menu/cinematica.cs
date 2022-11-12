99979cb99907a52fe28f5de150a8b93dd8ceafd1
pink3338 <112918472+pink3338@users.noreply.github.com>
2022-11-12 02:38:58 -0600

    cinematica

---
+++ Assets/Scripts/Menu/cinematica.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class cinematica : MonoBehaviour
+{
+    public void loadScene(string sceneName)
+    { 
+      SceneManager.LoadScene(sceneName);
+    }
+}
