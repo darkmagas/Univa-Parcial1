5830dc1ec1495df01e18337efa526353270176c7
Alejandralco3D <73479725+Alejandralco3D@users.noreply.github.com>
2022-11-14 08:51:15 -0600

    SAVE

---
+++ Assets/Scripts/Nuevo/BasicSave.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]public struct BasicSave 
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public int level;
+    public string mapName;
+}
---
+++ Assets/Scripts/Nuevo/SaveLoad.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using MagasLib;
+
+public class SaveLoad : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+    private int counter = 0;
+    private BasicSave _save;
+    private void Start()
+    {
+        var a = SaveData.Load<BasicSave>("data");
+        if (a == null)
+        {
+            text.SetText("NO SAVE DATA");
+            _save = new BasicSave
+            {
+                x = transform.position.x y = transform.position.y, z = transform.position.z,
+                level = 1, mapName = "School Parkinglot"
+            };
+            SaveData.Save(_save, "data");
+        }
+        else
+        {
+            _save = a.Value;
+            text.SetText($"SaveData :: { _save.x} {_save.y}" * $"{_save.z} Level :: {_save.level} Map {_save.mapName}");
+        }
+
+        //counter = PlayerPrefs.GetInt("TimesOpened", 0);
+        //counter++;
+        //text.SetText($"Times Open :: {counter}");
+        //PlayerPrefs.SetInt("TimesOpened", counter);
+        //PlayerPrefs.SetFloat("PlaterX", transform.x);
+    }
+}
